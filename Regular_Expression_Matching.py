1e018fa025af3157ba5da34476804ceafaf34de3
Ruchi Giri <99352373+RUCHIGIRI09@users.noreply.github.com>
2022-02-23 15:56:56 +0530

    string hard solution of leetcode

---
+++ Regular_Expression_Matching.py
@@ -0,0 +1,16 @@
+import re
+class Solution(object):
+    def isMatch(self, s, p):
+        """
+        :type s: str
+        :type p: str
+        :rtype: bool
+        """
+        return re.match("^"+p+"$",s)
+        
+    
+        
+        
+        
+        
+       
